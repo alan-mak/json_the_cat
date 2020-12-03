fc7b39f4e83bc6c8e31dfa651afcf85d4c3f6e68
Alan <alanmak95@gmail.com>
2020-12-03 19:48:03 +0000

    Refactor the breedFetcher file into two files

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,13 +1,21 @@
 const request = require('request');
-let breed = process.argv.slice(2);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (err, request, body) => {
-  if (err) throw new Error(err);
 
-  const data = JSON.parse(body);
-  if (data[0] === undefined) {
-    console.log("Breed not found");
-  } else {
-    console.log("Cat Discription:", data[0].description);
-  }
-});
+const fetchBreedDescription = (breed, callback) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (err, request, body) => {
+    if (err) {
+      callback(err, null);
+      return;
+    }
+
+    const data = JSON.parse(body);
+    if (data[0] === undefined) {
+      callback("Breed not found", null);
+    } else {
+      callback(null, data[0].description);
+    }
+  });
+};
+
+
+module.exports = { fetchBreedDescription };
