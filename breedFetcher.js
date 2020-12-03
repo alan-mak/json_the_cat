2c67f02787b54a59d734c2d2ca2dadb34b2fcd74
Alan <alanmak95@gmail.com>
2020-12-03 19:07:50 +0000

    Eslint Fix

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,13 +1,13 @@
 const request = require('request');
-breed = process.argv.slice(2);
+let breed = process.argv.slice(2);
 
 request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (err, request, body) => {
-  if(err) throw new Error(err);
+  if (err) throw new Error(err);
 
   const data = JSON.parse(body);
   if (data[0] === undefined) {
     console.log("Breed not found");
   } else {
-    console.log("Cat Discription:", data[0].description)
+    console.log("Cat Discription:", data[0].description);
   }
-})
+});
